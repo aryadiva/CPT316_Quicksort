d4fc4033a3496255447bcfc8421560d600548a73
aryadiva <aryadivap@student.usm.my>
2022-11-15 21:24:29 +0800

    Initial commit

---
+++ Quicksort.py
@@ -0,0 +1,76 @@
+import os as p
+import timeit
+
+# for timing the run time
+start = timeit.default_timer()
+
+# Quick sort in Python
+
+# file processing -------------------------------------------------
+def datasrc():
+  #reading fromfile
+  f = open('Data.txt', 'r')
+  data=f.readlines()
+  f.close()
+  return data
+
+#outputting data
+def dataoutput(input):
+  #checking if destination file existed or not
+  file_status=p.path.exists('Quick_Output.txt')
+  if(file_status == True):
+    p.remove('Quick_Output.txt')
+
+  #outputting to .txt file
+  with open('Quick_Output.txt', 'w') as f1:
+    f1.writelines(input)
+  f1.close
+
+#creating a variable based on the return value of function
+data = datasrc()
+
+#-------------------------------------------------------------------
+def partitioning(arr, start, end):
+  pivot = arr[start]
+  lowindex = start + 1
+  highindex = end
+
+  while True:
+    # If the current value is larger than the pivot
+    # it's on the right side of the pivot and we can move left, to the next element.
+    # We also need to make sure we haven't surpassed the low pointer, since that
+    # indicates we have already moved all the elements to the correct side of the pivot
+    while lowindex <= highindex and arr[highindex] >= pivot:
+      highindex = highindex - 1
+
+    # Opposite process from above
+    while lowindex <= highindex and arr[lowindex] <= pivot:
+      lowindex = lowindex + 1
+
+    # We will either found a value for both high and low that is out of order
+    # or low is higher than high, in which case we ca exit the loop
+    if lowindex <= highindex:
+      arr[lowindex], arr[highindex] = arr[highindex], arr[lowindex]
+      # The loop continues if above conditions persist
+    else:
+      # We exit the loop
+      break
+
+  arr[start], arr[highindex] = arr[highindex], arr[start]
+  return highindex;
+
+def quick_sort(arr, start, end):
+  if start >= end:
+    return
+
+  p = partitioning(arr, start, end)
+  quick_sort(arr, start, p-1)
+  quick_sort(arr, p+1, end)
+#--------------------------------------------------------------------
+
+quick_sort(data, 0, len(data)-1)
+dataoutput(data)
+
+# display the runtime
+stop = timeit.default_timer()
+print('Run Time: ', stop - start)
